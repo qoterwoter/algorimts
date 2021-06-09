423dc444971f88d0d709f080103bda838934ceaf
qoterwater <marderics@gmail.com>
2021-06-10 01:26:56 +0300

    m

--- heapSort/app.js
+++ heapSort/app.js
@@ -6,28 +6,28 @@ let sortArray = function(array) {
     if(array.length < 2) return array;
     let length = array.length;
     for(let i = Math.floor(length/2-1); i >= 0; i--) {
-        heap_sort(array, i, length);
+        heapify(array, i, length);
     }
     for(let j = length-1; j >= 0; j--) {
         [array[j], array[0]] = [array[0], array[j]];
-        heap_sort(array, 0, j);
+        heapify(array, 0, j);
     }
     return array;
 }
 
-let heap_sort = (array, i, length) => {
+let heapify = (array, i, length) => {
     let biggest = i;
     let left_child = i * 2 + 1;
 	let right_child = left_child + 1;
     if(left_child < length && array[biggest] < array[left_child]) biggest = left_child;
     if(right_child < length && array[biggest] < array[right_child]) biggest = right_child;
-    if(biggest > i) {
+    if(biggest != i) {
         [array[biggest], array[i]] = [array[i], array[biggest]];
-        heap_sort(array, biggest, length);
+        heapify(array, biggest, length);
     }
 }
 
-for (let i = 0; i < data.length-1;i++) {
+for (let i = 0; i < data.length-5;i++) {
     let start = new Date().getTime();
     let arr = data[i].split(', ').map(item=>parseInt(item))
     arr = sortArray(arr)
